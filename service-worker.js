6bed320faf3182923914637cddd8a383a962b1b8
Izabela Turaj <iza@izabelas-macbook-pro-2.home>
2018-12-21 09:00:27 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/traveltheme/precache-manifest.a4a3a9a364fc4050c740e35295fbece0.js"
+  "/traveltheme/precache-manifest.badf5d1c8ab0965a7f378e1e22477428.js"
 );
 
 workbox.clientsClaim();
